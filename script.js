11873074caaa6d2964caf253bec7bc50356dc1cf
ali <ali@example.com>
2025-04-26 21:00:40 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+let $ = document
+
+const userInputElem = $.querySelector("input")
+const searchBtnElem = $.querySelector(".search-btn")
+const iconElem = $.querySelector(".icon")
+const weatherElem = $.querySelector(".weather")
+const tempElem = $.querySelector(".temp")
+const minMaxElem = $.querySelector(".min-max")
+const cityElem = $.querySelector(".city")
+const countryElem = $.querySelector(".country")
+const dayElem = $.querySelector(".day")
+const dateElem = $.querySelector(".date")
+const msgElem = $.querySelector(".msg")
+
+const feelsLikeElem = $.querySelector(".feels-like")
+const humidityElem = $.querySelector(".humidity")
+const windSpeedElem = $.querySelector(".wind-speed")
+const pressureElem = $.querySelector(".pressure")
+
+// let daysName = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+// let monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const weatherStatus = {
+    clear: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ede21a" d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>',
+    // night: '<svg xmlns="http://www.w3.org/2000/svg" height="14" width="10.5" viewBox="0 0 384 512"><path fill="#74C0FC" d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>',
+    clouds: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#74C0FC" d="M0 336c0 79.5 64.5 144 144 144l368 0c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z"/></svg>',
+    wind: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#74C0FC" d="M288 32c0 17.7 14.3 32 32 32l32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128c-17.7 0-32 14.3-32 32s14.3 32 32 32l320 0c53 0 96-43 96-96s-43-96-96-96L320 0c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32l32 0c53 0 96-43 96-96s-43-96-96-96L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32zM128 512l32 0c53 0 96-43 96-96s-43-96-96-96L32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32s14.3 32 32 32z"/></svg>',
+    snow: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M224 0c17.7 0 32 14.3 32 32l0 30.1 15-15c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-49 49 0 70.3 61.4-35.8 17.7-66.1c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4l-5.2 19.3 23.6-13.8c15.3-8.9 34.9-3.7 43.8 11.5s3.8 34.9-11.5 43.8l-25.3 14.8 21.7 5.8c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-67.7-18.1L287.5 256l60.9 35.5 67.7-18.1c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4l-21.7 5.8 25.3 14.8c15.3 8.9 20.4 28.5 11.5 43.8s-28.5 20.4-43.8 11.5l-23.6-13.8 5.2 19.3c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-17.7-66.1L256 311.7l0 70.3 49 49c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-15-15 0 30.1c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-30.1-15 15c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l49-49 0-70.3-61.4 35.8-17.7 66.1c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4l5.2-19.3L48.1 395.6c-15.3 8.9-34.9 3.7-43.8-11.5s-3.7-34.9 11.5-43.8l25.3-14.8-21.7-5.8c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l67.7 18.1L160.5 256 99.6 220.5 31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4l21.7-5.8L15.9 171.6C.6 162.7-4.5 143.1 4.4 127.9s28.5-20.4 43.8-11.5l23.6 13.8-5.2-19.3c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l17.7 66.1L192 200.3l0-70.3L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l15 15L192 32c0-17.7 14.3-32 32-32z"/></svg>',
+    rain: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#74C0FC" d="M96 320c-53 0-96-43-96-96c0-42.5 27.6-78.6 65.9-91.2C64.7 126.1 64 119.1 64 112C64 50.1 114.1 0 176 0c43.1 0 80.5 24.3 99.2 60c14.7-17.1 36.5-28 60.8-28c44.2 0 80 35.8 80 80c0 5.5-.6 10.8-1.6 16c.5 0 1.1 0 1.6 0c53 0 96 43 96 96s-43 96-96 96L96 320zM81.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6S-3.3 490.7 1.9 478.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm120 0c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm244.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6s17.8 19.3 12.6 31.5zM313.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6z"/></svg>'
+}
+userInputElem.focus()
+const search = (event) => {
+    if (userInputElem.value.trim() == "") {
+        msgElem.innerHTML = "enter city or country"
+        msgElem.classList.add('msgShow');
+        setTimeout(() => {
+            msgElem.classList.remove('msgShow');
+        }, 3000);
+    } else {
+        if (navigator.onLine) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInputElem.value}&appid=9199964b9d502c597c01739c509e0742`)
+                .then(response => {
+                    if (response.status == 200) {
+                        console.log('here 1');
+                        return response
+                    } else {
+                        console.log('here');
+                    }
+                })
+                .then(response => response.json())
+                .then(response => insertToDom(response))
+                .catch(error => {
+                    console.log(msgElem.innerHTML);
+                    msgElem.innerHTML = `location not found`
+                    msgElem.classList.add('msgShow');
+                    setTimeout(() => {
+                        msgElem.classList.remove('msgShow');
+                    }, 3000);
+                })
+        } else {
+            msgElem.innerHTML = "check your connection"
+            msgElem.classList.add('msgShow');
+            setTimeout(() => {
+                msgElem.classList.remove('msgShow');
+            }, 3000);
+        }
+    }
+}
+const inputLocation = event => {
+    if (event.keyCode == 13) {
+        search(event)
+    }
+}
+const insertToDom = data => {
+    for (let i in weatherStatus) {
+        if (i == data.weather[0].main.toLowerCase()) {
+            iconElem.innerHTML = weatherStatus[i]
+        }
+    }
+    const now = new Date
+    let [day, date, month, year] = now.toUTCString().split(' ')
+    weatherElem.innerHTML = data.weather[0].main
+    tempElem.innerHTML = `${Math.floor(data.main.temp - 272.15)}c`
+    minMaxElem.innerHTML = `${Math.floor(data.main.temp_min - 272.15)}/${Math.floor(data.main.temp_max - 272.15)}`
+
+    cityElem.innerHTML = `${data.name}`
+    countryElem.innerHTML = `${data.sys.country}`
+    dayElem.innerHTML = `${day}`
+    dateElem.innerHTML = `${date}/${month}/${year}`
+
+    feelsLikeElem.innerHTML = `feels like: ${Math.floor(data.main.temp - 272.15)}c`
+    humidityElem.innerHTML = `humidity: ${data.main.humidity}%`
+    windSpeedElem.innerHTML = `wind speed: ${data.wind.speed}`
+    pressureElem.innerHTML = `pressure: ${data.main.pressure}`
+}
+userInputElem.addEventListener("keydown", inputLocation)
+searchBtnElem.addEventListener("click", search)
